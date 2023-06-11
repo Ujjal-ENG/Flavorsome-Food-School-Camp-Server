b882e49c4f090fdb873faefa2a258b8542f64bc0
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-11 16:52:19 +0600

    vercel problem fixed

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         // collection and db name
         const paymentCollections = client.db('FlavorsomeFoodSchool').collection('Payments');
